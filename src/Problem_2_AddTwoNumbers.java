55d27b5a55b69fa0f736acba4516cc58970380e1
fevzi <fevzifurkan2012@gmail.com>
2023-08-29 00:55:14 -0400

    problem 2 Add two numbers is added

---
+++ src/Problem_2_AddTwoNumbers.java
@@ -0,0 +1,23 @@
+public class Problem_2_AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int n1 = 0, n2 = 0,kat=1;
+        while (l1 != null || l2 != null) {
+            if (l1 != null) {
+                n1 += kat* l1.val;
+                l1=l1.next;
+            }
+            if (l2 != null) {
+                n2 += kat* l2.val;
+                l2 = l2.next;
+            }
+            kat*=10;
+        }
+        String sum = String.valueOf(n1 + n2);
+        ListNode result = null;
+        for (int i = 0; i <sum.length(); i++){
+            result = new ListNode((Integer.parseInt(String.valueOf((sum.charAt(i))))), result);
+
+        }
+        return result;
+    }
+}
