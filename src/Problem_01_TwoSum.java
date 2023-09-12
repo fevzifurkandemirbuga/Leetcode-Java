d63f119304f6dcdc23e6a07cf38de96d89267bf0
fevzi <fevzifurkan2012@gmail.com>
2023-09-11 22:58:00 -0300

    easy

--- src/Problem_01_TwoSum.java
+++ src/Problem_01_TwoSum.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Problem_1_TwoSum {
+public class Problem_01_TwoSum {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
